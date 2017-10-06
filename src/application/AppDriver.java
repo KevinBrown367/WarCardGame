870b127b947f932c70d2e4e33fbc0ac501a49336
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2017-10-05 19:18:21 -0600

    assignment 1 shit

--- src/application/AppDriver.java
+++ src/application/AppDriver.java
@@ -10,23 +10,24 @@ import java.awt.EventQueue;
 
 /**
  *
- * @author 645011
+ * @author Cameron Steinburg, Kevin Brown Solution provided in part by
+ * http://pirate.shu.edu/~wachsmut/Teaching/CSAS2214/Virtual/Lectures/chat-client-server.html
  */
 public class AppDriver {
-    
+
     public static void main(String[] args) {
-        
-        		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI window = new GUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-                  
-                        new GUI();
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GUI window = new GUI();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        new GUI();
     }
 }
